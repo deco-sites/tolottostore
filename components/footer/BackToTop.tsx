252cb5d71deda0252426a58e6e8a8c8a8e2f4ade
Lucas Fernandes Tolotto <lucas.tolotto@rocky.ag>
2023-12-11 14:59:02 -0300

    test commit

--- components/footer/BackToTop.tsx
+++ components/footer/BackToTop.tsx
@@ -6,7 +6,7 @@ export default function BackToTop({ content }: { content?: string }) {
       {content && (
         <div class="w-full flex items-center justify-center">
           <a href="#top" class="btn">
-            {content} <Icon id="ChevronUp" width={24} height={24} />
+            {content} <Icon id="ChevronUp" width={28} height={24} />
           </a>
         </div>
       )}
